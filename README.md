ea8f4f5b5d442a15123a0f9381a6180ea9834b5e
nostromo76 <ivans1976@gmail.com>
2023-07-04 16:04:52 +0200

    Readme file, env and some updates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Crypto-currency-converter
 Selenium tested Crypto-currency-converter small script with js html and css
+
+
+
+
+Prerequisites
+Before you can run the application and the tests, please make sure you have the following installed:
+
+Node.js
+npm (Node Package Manager)
+Installation
+To install the dependencies for this project, run the following command:
+
+npm install
+This will install the necessary packages such as Selenium, Selenium Webdriver, Mocha, and Mochawesome.
+
+Usage
+To run the application, open the Water_Consumption_ Calculator.html file in your web browser.
+
+To run the tests, use the following command:
+
+npm test
+This runs the Mocha tests with a 10-second timeout and generates a Mochawesome report. The --reporter mochawesome option in the package.json file enables this report.
+
+To change the browser used for testing, update the USE_BROWSER variable in the .env file to your desired browser name (e.g. 'chrome', 'firefox', or 'safari'). By default, the application uses Chrome. USE_BROWSER = 'YOUR BROWSER' driver = new Builder().forBrowser(process.env.USE_BROWSER).build()
+
+Contributing
+If you find any issues or have suggestions for improvements, please submit them as issues on this project's Github repository. We welcome contributions to this project!
--- test/Currency.test.js
+++ test/Currency.test.js
@@ -1,9 +1,11 @@
 const { Builder, until } = require('selenium-webdriver');
 const { Driver } = require('selenium-webdriver/chrome');
+const { Options } = require('selenium-webdriver/chrome');
 const { By } = require('selenium-webdriver');
 const { Select } = require('selenium-webdriver');
 const CurrencyPage = require('../pages/BasePage.js')
 const assert = require('assert');
+require('dotenv').config();
 
 const path = require('path');
 
@@ -14,13 +16,15 @@ const fileUrl = `file://${absoluteFilePath}`;
 describe('Test Currency converter', async function () {
     let driver;
     let CurrencyConverter
+    let options = new Options();
     beforeEach('Open Currency converter', async function () {
-        driver = new Builder().forBrowser('chrome').build()
+        driver = new Builder().forBrowser(process.env.USE_BROWSER).build();
+
         CurrencyConverter = new CurrencyPage(driver)
 
     })
     afterEach('Close browser', function () {
-        // CurrencyConverter.closeBrowser();
+        //CurrencyConverter.closeBrowser();
     })
     it('Open page', async function () {
 
@@ -45,63 +49,67 @@ describe('Test Currency converter', async function () {
         assert('label', 'Currency');
     })
 
-    it('Test choosing currency by option', async function () {
+    it.only('Test choosing currency by option', async function () {
         await CurrencyConverter.visit(fileUrl);
-
-        let select = await driver.findElement(By.id('currency'));
-        let currency = new Select(select);
         await driver.manage().window().maximize();
-        currency.selectByValue("USD");
-        let selectedOption = await currency.getSelectedOption();
-        assert.equal(await selectedOption.getAttribute("value"), "USD");
-    });
-    it('Test disabled option', async () => {
-        await CurrencyConverter.visit(fileUrl);
-
-        const select = await driver.findElement(By.id('cryptocurrency'));
-        const option = await select.findElement(By.css('option[value=""]'));
-
-
-        await option.click();
-
-
-        const selectedOption = await select.findElement(By.css('option:checked'));
-        assert.strictEqual(await selectedOption.getAttribute('value'), '', 'Option not selected');
-    });
-
-    it('Should display a message when button is clicked on empty inputs ', async () => {
-        await CurrencyConverter.visit(fileUrl);
-
-
-        const button = await driver.findElement(By.css('button[name="action"]'));
-        await button.click();
-
-        // cant find , wait for 5000ms
-        const messageElement = await driver.findElement(By.css('.messages'));
-        await driver.wait(until.elementIsVisible(messageElement), 5000);
-
-        // assert message
-        const messageText = await messageElement.getText();
-        assert.notStrictEqual(messageText, '', 'All the fields are mandatory');
-    });
-
-    it.only('Test site logic is failed', async function () {
-        await CurrencyConverter.visit(fileUrl);
-
-        let select = await driver.findElement(By.id('currency'));
+        let select = await driver.wait(until.elementLocated(By.id('currency')), 5000);
         let currency = new Select(select);
-        currency.selectByValue("THB");
-        const button = await driver.findElement(By.css('button[name="action"]'));
-        await button.click();
-        const messageElement = await driver.findElement(By.css('.messages'));
-        await driver.wait(until.elementIsVisible(messageElement), 5000);
+        currency.selectByValue('MXN');
 
-        // assert message
-        const messageText = await messageElement.getText();
-        assert.notStrictEqual(messageText, '', 'All the fields are mandatory');
+        // Get the currently selected option and verify its value
+        const selectedOption = await currency.getFirstSelectedOption();
+        const selectedOptionText = await selectedOption.getText();
+        assert.equal(selectedOptionText, 'MXN', 'Mexican Peso');
+    });
 
-    })
 });
+it('Test disabled option', async () => {
+    await CurrencyConverter.visit(fileUrl);
+
+    const select = await driver.findElement(By.id('cryptocurrency'));
+    const option = await select.findElement(By.css('option[value=""]'));
+
+
+    await option.click();
+
+
+    const selectedOption = await select.findElement(By.css('option:checked'));
+    assert.strictEqual(await selectedOption.getAttribute('value'), '', 'Option not selected');
+});
+
+it('Should display a message when button is clicked on empty inputs ', async () => {
+    await CurrencyConverter.visit(fileUrl);
+
+
+    const button = await driver.findElement(By.css('button[name="action"]'));
+    await button.click();
+
+    // cant find , wait for 5000ms
+    const messageElement = await driver.findElement(By.css('.messages'));
+    await driver.wait(until.elementIsVisible(messageElement), 5000);
+
+    // assert message
+    const messageText = await messageElement.getText();
+    assert.notStrictEqual(messageText, '', 'All the fields are mandatory');
+});
+
+it('Test site logic is failed', async function () {
+    await CurrencyConverter.visit(fileUrl);
+
+    let select = await driver.findElement(By.id('currency'));
+    let currency = new Select(select);
+    currency.selectByValue("THB");
+    const button = await driver.findElement(By.css('button[name="action"]'));
+    await button.click();
+    const messageElement = await driver.findElement(By.css('.messages'));
+    await driver.wait(until.elementIsVisible(messageElement), 5000);
+
+    // assert message
+    const messageText = await messageElement.getText();
+    assert.notStrictEqual(messageText, '', 'All the fields are mandatory');
+
+})
+
 
 
 
